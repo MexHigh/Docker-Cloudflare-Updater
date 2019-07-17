5366dc8f1f4e241ff5c6d2be7bec405202731ca0
Leon Schmidt <mail@leon.wtf>
2019-07-17 13:51:01 +0200

    print statements auskommentiert

--- image_src/healthcheck.py
+++ image_src/healthcheck.py
@@ -3,8 +3,8 @@ import sys
 
 for process in psutil.process_iter():
     if process.cmdline() == ['python3', 'cfupdater.py']:
-         print("0")
+         #print("0")
          sys.exit(0)
 
-print("1")
+#print("1")
 sys.exit(1)
